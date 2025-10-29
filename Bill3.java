e8f86478d570bc2f8330ca78a9139265077d6be6
Ofek Mossinson <ofekmoss@Ofeks-MacBook-Pro-2.local>
2025-10-29 19:33:25 +0200

    Fix - bill3 string

--- Bill3.java
+++ Bill3.java
@@ -7,6 +7,6 @@ public class Bill3 {
 		Integer billAmount = Integer.parseInt(args[3]);
 		Double billAmountPerDiner = Math.ceil(billAmount / 3.0);
 		
-		System.out.println(String.format("Dear %s, %s, and %s: pay %s shekels each.", name3, name2, name1, billAmountPerDiner));
+		System.out.println(String.format("Dear %s, %s, and %s: pay %s Shekels each.", name3, name2, name1, billAmountPerDiner));
 	}
 }
