f361b34d0e0fb60421a245a55f5c5abafad044ab
Ofek Mossinson <ofekmoss@Ofeks-MacBook-Pro-2.local>
2025-10-29 18:14:10 +0200

    Question 1 - bill3

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,14 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		java.io.Console console = System.console();
+
+	    String name1 = console.readLine("Enter the first diner's name: ");
+	    String name2 = console.readLine("Enter the second diner's name: ");
+	    String name3 = console.readLine("Enter the third diner's name: ");
+		Integer billAmount = Integer.parseInt(console.readLine("Enter the total bill amount: "));
+		Double billAmountPerDiner = Math.ceil(billAmount / 3.0);
+		
+		System.out.println(String.format("Dear %s, %s, and %s: pay %s shekels each.", name3, name2, name1, billAmountPerDiner));
 	}
 }
